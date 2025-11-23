bf1dd391ca1018e51cab4b3d36dfa3f572e8740e
barazd <barazd@gmail.com>
2025-11-23 20:01:18 +0100

    add: migráció, model, relációk, anonymous user

---
+++ app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    use SoftDeletes;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['subject', 'message', 'email', 'user_id'];
+
+    /**
+     * Get the user that owns the comment.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class)->withDefault([
+            $user->name = 'Anonymous',
+            $user->email = $this->email,
+        ]);
+    }
+
+} 
